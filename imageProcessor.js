880d253f09088c48d6506e031627b059a936741b
Muhammad Aryo Muzakki <aryo.muzakki@gmail.com>
2023-09-02 12:37:52 +0700

    try use other template

--- imageProcessor.js
+++ imageProcessor.js
@@ -3,50 +3,63 @@ const fs = require('fs');
 const CERTIFICATE_CONFIG_LOCATION = './certJsons/certJson.json';
 
 
-function printOnImage(image , label_placeholders, props, index , callback){
-        
+function printOnImage(image, label_placeholders, props, index, callback) {
+
     let label_placeholder = label_placeholders[index];
-    
+
     Jimp.loadFont(label_placeholder.font_url).then(font => {
-        
+
         let textObj = {};
         //textObj.text = label_placeholder.label
-        textObj.text = props[label_placeholder.param_name];
-        if(label_placeholder.alignmentX)
+        textObj.text = props[label_placeholder.param_name] || label_placeholder.label;
+        if (label_placeholder.alignmentX)
             textObj.alignmentX = Jimp[label_placeholder.alignmentX]
-        if(label_placeholder.alignmentY)
+        if (label_placeholder.alignmentY)
             textObj.alignmentY = Jimp[label_placeholder.alignmentY]
-        
+
+        console.log(textObj.text)
+
         image.print(
-                font,
-                label_placeholder.x,
-                label_placeholder.y,
-                textObj,
-                (label_placeholder.maxWidth||undefined)
+            font,
+            label_placeholder.x,
+            label_placeholder.y,
+            textObj,
+            // {
+            //     text: textObj.text,
+            //     alignmentX: textObj.alignmentX,
+            //     alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+            // },
+            (label_placeholder.maxWidth || undefined),
+            (label_placeholder.maxHeight || undefined),
         );
 
-        if(label_placeholders[index+1])
-            printOnImage(image , label_placeholders ,props, index+1 , callback)
+        if (label_placeholders[index + 1])
+            printOnImage(image, label_placeholders, props, index + 1, callback)
         else
-            callback(image , label_placeholder);
+            callback(image, label_placeholder);
     });
 }
-  
-function getCertJson(tmpl_id){
-let rawCertJson = fs.readFileSync(CERTIFICATE_CONFIG_LOCATION);
+
+function getCertJson(tmpl_id) {
+    let rawCertJson = fs.readFileSync(CERTIFICATE_CONFIG_LOCATION);
     let certJson = JSON.parse(rawCertJson);
     return certJson;
 }
 
-function generateImageFromJson(props, tmpl_id,  cb){
-    
+function generateImageFromJson(props, tmpl_id, cb) {
+
     const certJson = getCertJson(tmpl_id);
-    
+
+    console.log(certJson)
+
     Jimp.read(certJson.certificate_template)
         .then(image => {
 
-            printOnImage(image, certJson.label_placeholders, props , 0 , function(image ,label_placeholder){
-                image.write(certJson.certificate_template_out, function(){
+            console.log("width: ", image.bitmap.width)
+            console.log("height: ", image.bitmap.height)
+
+            printOnImage(image, certJson.label_placeholders, props, 0, function (image, label_placeholder) {
+                image.write(certJson.certificate_template_out, function () {
                     cb(certJson.certificate_template_out);
                 });
 
@@ -55,15 +68,15 @@ function generateImageFromJson(props, tmpl_id,  cb){
 }
 
 module.exports = {
-    generateImage:generateImageFromJson
+    generateImage: generateImageFromJson
 }
 
 
 
 
-  
 
 
 
 
-  
+
+
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -19,18 +19,20 @@ app.get('/', function (req, res) {
 })
 
 app.get('/generateCertificate', function (req, res) {
-    let { user_name, date , course , tmpl_id , response} = req.query;
-    let props = { user_name, date , course };
-    
-    imageProcessor.generateImage(props, tmpl_id ,function(imageUrl){
-        
-        if(response === "view")
-            res.send("<img src='"+imageUrl.replace("./outputImage","")+"' />");
-        else  
-            res.sendFile(imageUrl , { root: __dirname });  
-        
-    })
-    
+  let { user_name, date, course, tmpl_id, response } = req.query;
+  let props = { user_name, date, course };
+
+  imageProcessor.generateImage(props, tmpl_id, function (imageUrl) {
+
+    if (response === "view")
+      res.send("<img src='" + imageUrl.replace("./outputImage", "") + "' />");
+    else
+      res.sendFile(imageUrl, { root: __dirname });
+
+  })
+
 })
 
-app.listen(5000);
+const server = app.listen(5000, "127.0.0.1", () => {
+  console.log(`Listening at http://${server.address().address}:${server.address().port}`)
+});
